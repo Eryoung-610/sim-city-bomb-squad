6b15cfcb39d90e0cee188cb3b2b96872777e696f
Eryoung-610 <eryoung610@gmail.com>
2020-05-29 16:41:15 -0700

    Finished program

--- bomb.js
+++ bomb.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded",function (){
     // Game Logic Variables
     const STARTING_TIME = 30;
     let remainingTime = 0;
-    let gameOver = false;
+    let gameOver = true;
     let countdown = null; //Hold countdown interval
 
     let wiresToCut = [];
@@ -27,12 +27,22 @@ document.addEventListener("DOMContentLoaded",function (){
 
     function reset() {
         console.log("clicked reset");
+        body.classList.remove("flat");
+        for (let wire in wireState) {
+            wireState[wire] = false;
+        }
+        wiresToCut = [];
+        for(let i = 0; i < wireBox.children.length; i++) {
+            let color = wireBox.children[i].id;
+            wireBox.children[i].src = "img/uncut-" + color + "-wire.png";
+        }
+        
         init()
     }
 
     function init() {
         remainingTime = STARTING_TIME;
-        
+        gameOver = false;
         // Set wires to cut
         for(const color in wireState) {
             let randoNum = Math.random();
@@ -48,8 +58,25 @@ document.addEventListener("DOMContentLoaded",function (){
     }
 
     function wireClick(e) {
-        console.log("clicked wire box");
-        console.log(e.target);
+        console.log("You clicked " + e.target.id);
+        let color = e.target.id;
+
+        if(gameOver === false && wireState[color] === false) {
+            e.target.src = "img/cut-" + color + "-wire.png";
+            wireState[color] = true;
+            let wireIndex = wiresToCut.indexOf(color);
+            // If the wire has an index, it needs to be cut!
+            if(wireIndex > - 1){
+                console.log("Correct!")
+                wiresToCut.splice(wireIndex,1);
+                if (wiresToCut.length < 1) {
+                    endGame(true);
+                }
+            } else {
+                console.log("Bad news bears");
+                endGame(false);
+            }
+        }
     }
 
     function updateClock() {
